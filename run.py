04514555b1ab3132d1a5ff5bc66cc087cec27e2d
Pradyumna Kaushik <pkaushi1@binghamton.edu>
2016-12-02 14:34:22 -0500

    driver added to read in command line and initialize parameters

---
+++ run.py
@@ -0,0 +1,14 @@
+#!usr/bin/python
+
+import machine_learning_project
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 7:
+        print("Usage: run.py <input filename> <number of layers> <number of drop out layers> <optimizer> <learning rate> <momentum> <err_metric>")
+    else:
+        args = sys.argv
+        err_metric = None
+        if len(args) == 8 and args[7] != "":
+            err_metric = args[7]
+        machine_learning_project.run_nnet(args[1], int(args[2]), int(args[3]), args[4], float(args[5]), float(args[6]), err_metric)

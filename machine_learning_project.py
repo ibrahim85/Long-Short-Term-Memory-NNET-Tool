02e1557866764bd797650db957daea29bdba22d6
Pradyumna Kaushik <pkaushi1@binghamton.edu>
2016-12-02 14:51:08 -0500

    modularized -- put all the statements inside a run() method

--- machine_learning_project.py
+++ machine_learning_project.py
@@ -28,6 +28,7 @@ def run_nnet(input_filename, n_layers, n_dropout_layers, optimizer, learning_rat
 	parameters["momentum"] = momentum
 	if err_metric:
 		parameters["err_metric"] = err_metric
+	run()
 
 # convert an array of values into a dataset matrix
 def create_dataset(dataset, look_back=1):
@@ -37,67 +38,69 @@ def create_dataset(dataset, look_back=1):
 		dataX.append(a)
 		dataY.append(dataset[i + look_back, 0])
 	return numpy.array(dataX), numpy.array(dataY)
-# fix random seed for reproducibility
-numpy.random.seed(7)
-# load the dataset
 
-# changed this line to take in command line parameters
-# dataframe = pandas.read_csv('international-airline-passengers.csv', usecols=[1], engine='python', skipfooter=3)
-dataframe = pandas.read_csv(parameters["input_filename"], usecols=[1], engine='python', skipfooters=3)
+def run():
+	# fix random seed for reproducibility
+	numpy.random.seed(7)
+	# load the dataset
 
-dataset = dataframe.values
-dataset = dataset.astype('float32')
-# normalize the dataset
-scaler = MinMaxScaler(feature_range=(0, 1))
-dataset = scaler.fit_transform(dataset)
-# split into train and test sets
-train_size = int(len(dataset) * 0.67)
-test_size = len(dataset) - train_size
-train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
-# reshape into X=t and Y=t+1
-look_back = 1
-trainX, trainY = create_dataset(train, look_back)
-testX, testY = create_dataset(test, look_back)
-# reshape input to be [samples, time steps, features]
-trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
-testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
-# create and fit the LSTM network
-model = Sequential()
+	# changed this line to take in command line parameters
+	# dataframe = pandas.read_csv('international-airline-passengers.csv', usecols=[1], engine='python', skipfooter=3)
+	dataframe = pandas.read_csv(parameters["input_filename"], usecols=[1], engine='python', skipfooters=3)
 
-# changed this line to correspond to command line arguments.
-# model.add(LSTM(4, input_dim=look_back))
-model.add(LSTM(parameters["n_layers"], input_dim=look_back))
+	dataset = dataframe.values
+	dataset = dataset.astype('float32')
+	# normalize the dataset
+	scaler = MinMaxScaler(feature_range=(0, 1))
+	dataset = scaler.fit_transform(dataset)
+	# split into train and test sets
+	train_size = int(len(dataset) * 0.67)
+	test_size = len(dataset) - train_size
+	train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+	# reshape into X=t and Y=t+1
+	look_back = 1
+	trainX, trainY = create_dataset(train, look_back)
+	testX, testY = create_dataset(test, look_back)
+	# reshape input to be [samples, time steps, features]
+	trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+	testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+	# create and fit the LSTM network
+	model = Sequential()
 
-model.add(Dense(1))
+	# changed this line to correspond to command line arguments.
+	# model.add(LSTM(4, input_dim=look_back))
+	model.add(LSTM(parameters["n_layers"], input_dim=look_back))
 
-# changed this to take from command line
-# model.compile(loss='mean_squared_error', optimizer='adam')
-model.compile(loss=parameters["err_metric"], optimizer=parameters["optimizer"])
+	model.add(Dense(1))
 
-model.fit(trainX, trainY, nb_epoch=100, batch_size=1, verbose=2)
-# make predictions
-trainPredict = model.predict(trainX)
-testPredict = model.predict(testX)
-# invert predictions
-trainPredict = scaler.inverse_transform(trainPredict)
-trainY = scaler.inverse_transform([trainY])
-testPredict = scaler.inverse_transform(testPredict)
-testY = scaler.inverse_transform([testY])
-# calculate root mean squared error
-trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
-print('Train Score: %.2f RMSE' % (trainScore))
-testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
-print('Test Score: %.2f RMSE' % (testScore))
-# shift train predictions for plotting
-trainPredictPlot = numpy.empty_like(dataset)
-trainPredictPlot[:, :] = numpy.nan
-trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
-# shift test predictions for plotting
-testPredictPlot = numpy.empty_like(dataset)
-testPredictPlot[:, :] = numpy.nan
-testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
-# plot baseline and predictions
-plt.plot(scaler.inverse_transform(dataset))
-plt.plot(trainPredictPlot)
-plt.plot(testPredictPlot)
-plt.show()
+	# changed this to take from command line
+	# model.compile(loss='mean_squared_error', optimizer='adam')
+	model.compile(loss=parameters["err_metric"], optimizer=parameters["optimizer"])
+
+	model.fit(trainX, trainY, nb_epoch=100, batch_size=1, verbose=2)
+	# make predictions
+	trainPredict = model.predict(trainX)
+	testPredict = model.predict(testX)
+	# invert predictions
+	trainPredict = scaler.inverse_transform(trainPredict)
+	trainY = scaler.inverse_transform([trainY])
+	testPredict = scaler.inverse_transform(testPredict)
+	testY = scaler.inverse_transform([testY])
+	# calculate root mean squared error
+	trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+	print('Train Score: %.2f RMSE' % (trainScore))
+	testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+	print('Test Score: %.2f RMSE' % (testScore))
+	# shift train predictions for plotting
+	trainPredictPlot = numpy.empty_like(dataset)
+	trainPredictPlot[:, :] = numpy.nan
+	trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+	# shift test predictions for plotting
+	testPredictPlot = numpy.empty_like(dataset)
+	testPredictPlot[:, :] = numpy.nan
+	testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+	# plot baseline and predictions
+	plt.plot(scaler.inverse_transform(dataset))
+	plt.plot(trainPredictPlot)
+	plt.plot(testPredictPlot)
+	plt.show()
